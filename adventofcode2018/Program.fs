db97b31970dafc3995c3606b257394eebdb3acbb
Andreas Juch <juch.andreas@fronius.com>
2018-12-05 14:30:53 +0100

    day02

--- adventofcode2018/Program.fs
+++ adventofcode2018/Program.fs
@@ -6,6 +6,6 @@ module Main =
   [<EntryPoint>]
   let main argv =
     printfn "Solution of Day 1: %d" Day01.result
-
+    printfn "Solution of Day 2: %d" Day02.result
     Console.ReadKey() |> ignore
     0
---
+++ adventofcode2018/day02/solution.fs
@@ -0,0 +1,27 @@
+﻿namespace AdventOfCode
+
+module Day02 =
+
+  let boolToInt b = if b then 1 else 0
+
+  let countOccurences (charToFind:char) text =
+    let rec find characterList occurences =
+      match characterList with
+      | x :: tail -> find tail occurences + (x = charToFind |> boolToInt)
+      | [] -> occurences
+    find text 0
+
+  let containsCharacterNTimes n (text:char list) =
+    text |> List.distinct |> List.exists(fun c -> countOccurences c text = n)
+  let containsCharacterTwice = containsCharacterNTimes 2
+  let containsCharacterThrice = containsCharacterNTimes 3
+
+  let list = SimpleIO.readLines("day02\input.txt") |> Seq.toList
+  
+  let twiceOcurrences, thriceOccurrences =
+    list
+    |> List.map (fun s -> s.ToCharArray() |> Array.toList)
+    |> List.map (fun chars -> (containsCharacterTwice chars |> boolToInt, containsCharacterThrice chars |> boolToInt))
+    |> List.reduce (fun (a, b) (c, d) -> (a + c, b + d))
+
+  let result = twiceOcurrences * thriceOccurrences
